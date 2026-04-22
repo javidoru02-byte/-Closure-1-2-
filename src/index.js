5533d055ff1aea072e4c703a579df582cd5ab10a
David <javidoru02@gmail.com>
2026-04-23 01:18:39 +0300

    Add: code for second task

--- src/index.js
+++ src/index.js
@@ -10,3 +10,32 @@ function createLogger(prefix) {
 const authLogger = createLogger("AUTH: ");
 
 console.log(authLogger(`User logged in`));
+
+function createLimiter(limit) {
+  const LIMIT = limit;
+  let step = 0;
+
+  return function () {
+    if (Number.isNaN(LIMIT)) {
+      return 0;
+    }
+
+    if (typeof LIMIT === "number" && LIMIT >= 0) {
+      if (step < LIMIT) {
+        ++step;
+        return "Ok";
+      }
+      return "Error";
+    } else {
+      return 0;
+    }
+  };
+}
+
+const limited = createLimiter(3);
+
+console.log(limited());
+console.log(limited());
+console.log(limited());
+console.log(limited());
+
